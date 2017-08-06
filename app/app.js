1448fc972c4b0feca59295c1c54314ab15336f7f
Justin Mi <justin.mi@berkeley.edu>
2017-08-06 13:48:56 -0700

    created index page, modified index page routing to accomodate passport

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+// middleware
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,15 +6,30 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var flash = require('connect-flash');
+var passport = require('passport');
+var mongoose = require('mongoose'); 
+var session = require('express-session');
 
+// routes
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// create Express app instance
 var app = express();
+
+// server setup
+var configDB = require('./config/database.js');
+
+// connect to server
+mongoose.connect('mongodb://localhost/and-sold');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// pass passport for configuration. Uncomment when ready
+//require('./config/passport')(passport); 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -29,8 +45,15 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// set up initial route paths
 app.use('/users', users);
+index(app, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,6 +71,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(3000, function (req, res) {
+  console.log("listening on port 3000");
 });
 
 module.exports = app;
